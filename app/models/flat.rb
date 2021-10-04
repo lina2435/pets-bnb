f96e4e6d7e0ede4016588817bc35906b9f40c764
hayline solano <hsolano.more@gmail.com>
2021-10-04 18:18:33 -0500

    validation

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,5 @@
 class Flat < ApplicationRecord
+  has_many :bookings
+  belongs_to :user
+  validates :address, :description, :price_per_night, :number_of_pets, :title, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :bookings
+  validates :first_name, :last_name, :email, :address, :pet_name, presence: true
 end
