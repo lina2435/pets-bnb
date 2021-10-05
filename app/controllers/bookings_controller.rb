5bef65eb8cd877510a249e0b1d7ea04aad2d6042
Denise Hermoza <denise.hmm91@gmail.com>
2021-10-05 17:24:42 -0500

    booking create and new done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,51 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    @booking = Booking.find(params[:id])
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    if @booking.update(booking_params)
+      redirect_to flats_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.status = "pending"
+    if @booking.save
+      redirect_to user_booking_path(@booking, @user)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :user_id, :flat_id)
+  end
 end
