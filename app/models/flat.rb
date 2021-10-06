995cd146886f8a061890ce486f38dd739d5603b2
dajhely angles <assiaxramx10@gmail.com>
2021-10-06 13:39:14 -0500

    adding geocoding to the model Flat

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -3,4 +3,7 @@ class Flat < ApplicationRecord
   belongs_to :user, optional:true
   validates :address, :description, :price_per_night, :number_of_pets, :title, presence: true
   has_many_attached :photos
+  
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
