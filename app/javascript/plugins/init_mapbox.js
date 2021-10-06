1fa57dd5f99b87b86f999242933dfdac34a6e7c4
dajhely angles <assiaxramx10@gmail.com>
2021-10-06 15:00:42 -0500

    Adding map with markets

---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,22 @@
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+
+  if (mapElement) {
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10'
+    });
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(map);
+    });
+  }
+};
+
+export { initMapbox };
